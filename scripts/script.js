a23ab770b1c1628cf85fe7853a391ec5d0243849
claudiobentodala <programmer.claudio@gmail.com>
2025-04-20 11:47:52 +0100

    Incluido o icone fechar e menu no script

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,14 @@
+let menu = window.document.querySelector('img#menu')
+let fechar = window.document.querySelector('img#fechar')
 function clickMenu() {
      if (itens.style.display == 'none') {
-         itens.style.display = 'flex'
+        itens.style.display = 'flex'
+        menu.style.display = 'none'
+        fechar.style.display = 'block'
     } 
     else{
         itens.style.display = 'none'
+        menu.style.display = 'block'
+        fechar.style.display = 'none'
     }
 }
