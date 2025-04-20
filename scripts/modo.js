7e402bf5aa732f8e091873a9047ddfd8f0358a23
claudiobentodala <programmer.claudio@gmail.com>
2025-04-20 11:48:09 +0100

    Update modo.js
    Melhorado os modos

--- scripts/modo.js
+++ scripts/modo.js
@@ -1,19 +1,24 @@
-let ligth = window.document.querySelector('img#ligth')
-let dark = window.document.querySelector('img#dark')
 let fundo = window.document.querySelector('body')
 
-let link_home = {text: window.document.querySelector('a#texta'),
+let link_home = {
+    text: window.document.querySelector('a#texta'),
     get: window.document.querySelector('a#geta'),
     patro: window.document.querySelector('a#patroa'),
     ora: window.document.querySelector('a#ora'),
 }
 
+let icone = {
+    dark: window.document.querySelector('img#dark'),
+    ligth: window.document.querySelector('img#ligth')
+}
+
 function modo() {
     if (ligth.style.display == 'none') {
-        dark.style.display = 'none'
-        ligth.style.display = 'block'
+        
+        icone.dark.style.display = 'none'
+        icone.ligth.style.display = 'block'
         fundo.style.background = '#020235'
-
+        
         link_home.text.style.background = '#020235'
         link_home.text.style.color = 'skyblue'
 
@@ -25,8 +30,9 @@ function modo() {
 
         link_home.ora.style.background = '#020235'
         link_home.ora.style.color = 'skyblue'
-
+        
     } else {
+
         ligth.style.display = 'none'
         dark.style.display = 'block'
         
