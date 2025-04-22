5f64311fd70172c6b85e915eede2062c44955347
claudiobentodala <programmer.claudio@gmail.com>
2025-04-22 21:26:44 +0100

    sombras no modo ligth

--- scripts/modo.js
+++ scripts/modo.js
@@ -1,5 +1,15 @@
+/*
+CRIAR VARIAVEIS CONSTANTE QUE GUARDA AS CORES, PX...
+*/
 let fundo = window.document.querySelector('body')
 let modo_icone = window.document.querySelector('div#modo')
+
+let section_home = {
+    introducao: window.document.querySelector('section#intro'),
+    time: window.document.querySelector('section#time'),
+    estado: window.document.querySelector('section#status'),
+    get: window.document.querySelector('section#get')
+}
 
 let link_home = {
     text: window.document.querySelector('a#texta'),
@@ -20,28 +30,39 @@ function modo() {
         icone.ligth.style.display = 'block'
         fundo.style.background = '#020235'
         
+        section_home.introducao.style.boxShadow = '0px 0px 30px 22px darkblue'
         link_home.text.style.background = '#020235'
         link_home.text.style.color = 'skyblue'
-
+        
+        section_home.time.style.boxShadow = '0px 0px 30px 22px darkblue'
+        
+        section_home.estado.style.boxShadow = '0px 0px 30px 22px darkblue'
+        
+        section_home.get.style.boxShadow = '0px 0px 30px 22px darkblue'
         link_home.get.style.background = '#020235'
         link_home.get.style.color = 'skyblue'
-
+        
         link_home.patro.style.background = '#020235'
         link_home.patro.style.color = 'skyblue'
-
+        
         link_home.ora.style.background = '#020235'
         link_home.ora.style.color = 'skyblue'
-
     } else {
-
+        
         ligth.style.display = 'none'
         dark.style.display = 'block'
         
         fundo.style.background = '#b1d9ff'
         
+        section_home.introducao.style.boxShadow = '0px 0px 0px skyblue'
         link_home.text.style.background = 'skyblue'
         link_home.text.style.color = '#020235'
-
+        
+        section_home.time.style.boxShadow = '0px 0px 0px skyblue'
+        
+        section_home.estado.style.boxShadow = '0px 0px 0px skyblue'
+        
+        section_home.get.style.boxShadow = '0px 0px 0px skyblue'
         link_home.get.style.background = 'skyblue'
         link_home.get.style.color = '#020235'
 
