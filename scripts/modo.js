121f98752bd38e2447f051becb97b8dc9a410636
claudiobentodala <programmer.claudio@gmail.com>
2025-04-22 21:39:25 +0100

    terminado as sombres do modo claro da pagina inicial

--- scripts/modo.js
+++ scripts/modo.js
@@ -4,11 +4,18 @@ CRIAR VARIAVEIS CONSTANTE QUE GUARDA AS CORES, PX...
 let fundo = window.document.querySelector('body')
 let modo_icone = window.document.querySelector('div#modo')
 
+let icone = {
+    dark: window.document.querySelector('img#dark'),
+    ligth: window.document.querySelector('img#ligth')
+}
+
 let section_home = {
     introducao: window.document.querySelector('section#intro'),
     time: window.document.querySelector('section#time'),
     estado: window.document.querySelector('section#status'),
-    get: window.document.querySelector('section#get')
+    get: window.document.querySelector('section#get'),
+    orador: window.document.querySelector('section#ora'),
+    patrocinadores: window.document.querySelector('section#patro')
 }
 
 let link_home = {
@@ -17,12 +24,6 @@ let link_home = {
     patro: window.document.querySelector('a#patroa'),
     ora: window.document.querySelector('a#ora'),
 }
-
-let icone = {
-    dark: window.document.querySelector('img#dark'),
-    ligth: window.document.querySelector('img#ligth')
-}
-
 function modo() {
     if (ligth.style.display == 'none') {
         
@@ -42,11 +43,14 @@ function modo() {
         link_home.get.style.background = '#020235'
         link_home.get.style.color = 'skyblue'
         
+        section_home.orador.style.boxShadow = '0px 0px 30px 22px darkblue'
+        link_home.ora.style.background = '#020235'
+        link_home.ora.style.color = 'skyblue'
+        
+        section_home.patrocinadores.style.boxShadow = '0px 0px 30px 22px darkblue'
         link_home.patro.style.background = '#020235'
         link_home.patro.style.color = 'skyblue'
         
-        link_home.ora.style.background = '#020235'
-        link_home.ora.style.color = 'skyblue'
     } else {
         
         ligth.style.display = 'none'
@@ -65,13 +69,13 @@ function modo() {
         section_home.get.style.boxShadow = '0px 0px 0px skyblue'
         link_home.get.style.background = 'skyblue'
         link_home.get.style.color = '#020235'
-
-        link_home.patro.style.background = 'skyblue'
-        link_home.patro.style.color = '#020235'
-
+        
+        section_home.orador.style.boxShadow = '0px 0px 0px skyblue'
         link_home.ora.style.background = 'skyblue'
         link_home.ora.style.color = '#020235'
-
         
+        section_home.patrocinadores.style.boxShadow = '0px 0px 0px skyblue'
+        link_home.patro.style.background = 'skyblue'
+        link_home.patro.style.color = '#020235'
     }
 }
