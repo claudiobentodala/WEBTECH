4f7d08148a9c771c94f9fc65a4f906b9b0a03200
claudiobentodala <programmer.claudio@gmail.com>
2025-04-25 13:17:27 +0100

    Criado a secção de carregamento do site

---
+++ scripts/load.js
@@ -0,0 +1,22 @@
+function load(){
+    return true
+}
+function loaded(){
+    return true
+}
+let head = window.document.querySelector('header')
+let main = window.document.querySelector('main')
+let foot = window.document.querySelector('footer')
+let bod = window.document.querySelector('div#body')
+if(load()) {
+    head.style.display = 'none'
+    main.style.display = 'none'
+    foot.style.display = 'none'
+    bod.style.display = 'flex'
+}
+if(loaded()){
+    bod.style.display = 'none'
+    head.style.display = 'flex'
+    main.style.display = 'block'
+    foot.style.display = 'block'
+}
