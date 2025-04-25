e1bc05ebe8fb2862a23a2d1ecfb150433d773057
claudiobentodala <programmer.claudio@gmail.com>
2025-04-25 16:37:41 +0100

    criado com sucesso o LOAD

--- scripts/load.js
+++ scripts/load.js
@@ -1,22 +1,16 @@
-function load(){
-    return true
-}
-function loaded(){
-    return true
-}
-let head = window.document.querySelector('header')
-let main = window.document.querySelector('main')
-let foot = window.document.querySelector('footer')
-let bod = window.document.querySelector('div#body')
-if(load()) {
+var bod = window.document.querySelector('div#body')
+var head = window.document.querySelector('header')
+var main = window.document.querySelector('main')
+var foot = window.document.querySelector('footer')
+function carregar() {
     head.style.display = 'none'
     main.style.display = 'none'
     foot.style.display = 'none'
     bod.style.display = 'flex'
-}
-if(loaded()){
-    bod.style.display = 'none'
-    head.style.display = 'flex'
-    main.style.display = 'block'
-    foot.style.display = 'block'
+    setTimeout(function(){
+        bod.style.display = 'none'
+        head.style.display = 'flex'
+        main.style.display = 'block'
+        foot.style.display = 'block'
+    }, 3000)
 }
