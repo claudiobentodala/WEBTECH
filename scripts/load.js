00c0146830ec95dbab90fa99010a6a9422d6f0dd
claudiobentodala <programmer.claudio@gmail.com>
2025-04-26 13:39:47 +0100

    Mudada de variaveis normais à objecto

--- scripts/load.js
+++ scripts/load.js
@@ -1,20 +1,22 @@
-var bod = window.document.querySelector('div#body')
-var head = window.document.querySelector('header')
-var main = window.document.querySelector('main')
-var foot = window.document.querySelector('footer')
+var corpo = {
+    bod: window.document.querySelector('div#body'),
+    head: window.document.querySelector('header'),
+    main: window.document.querySelector('main'),
+    foot: window.document.querySelector('footer')
+}
 function carregar(completo,tempo) {
     if (completo) {
-        head.style.display = 'none'
+        corpo.head.style.display = 'none'
     }
-    main.style.display = 'none'
-    foot.style.display = 'none'
-    bod.style.display = 'flex'
+    corpo.main.style.display = 'none'
+    corpo.foot.style.display = 'none'
+    corpo.bod.style.display = 'flex'
     setTimeout(function(){
         if (completo) {
-            head.style.display = 'flex'
+            corpo.head.style.display = 'flex'
         }
-        main.style.display = 'block'
-        foot.style.display = 'block'
-        bod.style.display = 'none'
+        corpo.main.style.display = 'block'
+        corpo.foot.style.display = 'block'
+        corpo.bod.style.display = 'none'
     }, tempo)
 }
