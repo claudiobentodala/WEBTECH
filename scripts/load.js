8567b61f4aa2f85f85305476d18efbfa89cd02c6
claudiobentodala <programmer.claudio@gmail.com>
2025-04-26 13:21:24 +0100

    Melhorado o carregamento das paginas

--- scripts/load.js
+++ scripts/load.js
@@ -2,15 +2,19 @@ var bod = window.document.querySelector('div#body')
 var head = window.document.querySelector('header')
 var main = window.document.querySelector('main')
 var foot = window.document.querySelector('footer')
-function carregar() {
-    head.style.display = 'none'
+function carregar(completo,tempo) {
+    if (completo) {
+        head.style.display = 'none'
+    }
     main.style.display = 'none'
     foot.style.display = 'none'
     bod.style.display = 'flex'
     setTimeout(function(){
-        bod.style.display = 'none'
-        head.style.display = 'flex'
+        if (completo) {
+            head.style.display = 'flex'
+        }
         main.style.display = 'block'
         foot.style.display = 'block'
-    }, 3150)
+        bod.style.display = 'none'
+    }, tempo)
 }
