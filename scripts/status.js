618fb7bc07684be746d71411f7e51878e98ff66f
claudiobentodala <programmer.claudio@gmail.com>
2025-04-28 08:53:27 +0100

    Feita o script do status

---
+++ scripts/status.js
@@ -0,0 +1,6 @@
+var cursos = 1
+var turmas = 1
+var alunos = turmas * 15
+window.document.querySelector('p#alunos').innerText = alunos
+window.document.querySelector('p#turmas').innerText = turmas
+window.document.querySelector('p#cursos').innerText = cursos
