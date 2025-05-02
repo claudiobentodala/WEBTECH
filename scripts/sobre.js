88453403d99f5308f2c4b7525b0e2a8f743dd0bf
claudiobentodala <programmer.claudio@gmail.com>
2025-05-02 09:02:14 +0100

    Melhorado a pagina SOBRE O EVENTO com SCRIPT

--- scripts/sobre.js
+++ scripts/sobre.js
@@ -4,7 +4,9 @@ var elemento = {
     ano: window.document.querySelector('span#ano'),
     hora: window.document.querySelector('span#hora'),
     minuto: window.document.querySelector('span#minuto'),
-    local: window.document.querySelector('span#local')
+    local: window.document.querySelector('span#local'),
+    historia: window.document.querySelector('p#historia'),
+    localizacao: window.document.querySelector('p#localizacao')
 }
 var data = {
     ano: new Date().getFullYear(),
@@ -14,8 +16,12 @@ var data = {
 var passo = {
     comeco: 10,
     terminio: 17,
-    local: 'angola - luanda - camama - camama1'
+    local: 'angola - luanda - camama - camama1 - colegio madrugada',
+    descricao_historia:'O Instituto Politécnico Privado Carmun teve origem como o Colégio Madrugada, uma escola privada voltada ao ensino geral. Com o tempo, alinhando-se às necessidades crescentes do país por formação técnica e profissional, a instituição foi reestruturada e passou a operar como um instituto politécnico, com foco na educação técnica de nível médio.',
+    descricao_localizacao: 'Muito perto da instituição, a menos de 100 metros, encontra-se o espaço de eventos SOLAR DO CAMAMA. A uma curta distância a pé, a cerca de 250-300 metros, existem várias lojas e serviços, incluindo o supermercado Deskontão Camama, o Supermercado Rio Jordão e o Multicenter. Um pouco mais adiante, a cerca de 500-600 metros, localizam-se o supermercado Fresmart Express Camama II e o restaurante Paragem Quente. O Hotel Camama fica também nas imediações, a aproximadamente 900 metros de distância. Estes estabelecimentos na Estrada Direita do Camama podem ajudar na sua orientação para encontrar o instituto.'
 }
+elemento.historia.innerHTML = passo.descricao_historia
+elemento.localizacao.innerHTML = passo.descricao_localizacao
 elemento.nome.innerHTML = 'instituto politecnico carmun'
 elemento.fundacao.innerHTML = 2015
 if (data.mes >= 5) {
