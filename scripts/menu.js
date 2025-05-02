de7bfae72a55a404d94a11101102f510621e2e1b
claudiobentodala <programmer.claudio@gmail.com>
2025-05-02 15:55:13 +0100

    Inserida o fechamento do menu com click fora

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,16 +1,27 @@
+var elemento = {
+    principal: window.document.querySelector('main'),
+    rodape: window.document.querySelector('footer')
+}
 var estado = {
     menu : window.document.querySelector('img#menu'),
     fechar : window.document.querySelector('img#fechar')
 }
 function clickMenu() {
-     if (itens.style.display == 'none') {
+    if (itens.style.display == 'none') {
         itens.style.display = 'flex'
         estado.menu.style.display = 'none'
         estado.fechar.style.display = 'block'
     } 
     else{
         itens.style.display = 'none'
-        estado.menu.style.display = 'block'
         estado.fechar.style.display = 'none'
+        estado.menu.style.display = 'block'
     }
+}
+function foraMenu() {
+    if(itens.style.display == 'flex') {
+        itens.style.display = 'none'
+        estado.fechar.style.display = 'none'  
+        estado.menu.style.display = 'block'
+    } 
 }
