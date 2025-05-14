e31cbaa75d9d8be05e82047fec662ec153edb3d9
claudiobentodala <programmer.claudio@gmail.com>
2025-05-14 22:26:32 +0100

    INSERIDA O STATUS EM UMA FUNÇÃO

--- scripts/status.js
+++ scripts/status.js
@@ -1,6 +1,9 @@
-var cursos = 1
-var turmas = 1
-var alunos = turmas * 15
-window.document.querySelector('p#alunos').innerText = alunos
-window.document.querySelector('p#turmas').innerText = turmas
-window.document.querySelector('p#cursos').innerText = cursos
+function status() {
+    let alunos = 30
+    let cursos = 1
+    let turmas = 1
+    let participantes = turmas * alunos
+    window.document.querySelector('p#alunos').innerText = participantes
+    window.document.querySelector('p#turmas').innerText = turmas
+    window.document.querySelector('p#cursos').innerText = cursos
+}
